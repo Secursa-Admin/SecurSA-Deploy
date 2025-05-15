3767b606f305ebdeb55cf7d8f15ec7f9bc3e2f06
Secursa-Admin <support@secursa.co.za>
2025-05-15 16:17:29 +0200

    Delete src directory

--- src/index.js
+++
@@ -1,1 +0,0 @@
-import React from 'react'; import ReactDOM from 'react-dom/client'; import Homepage from './components/Homepage'; ReactDOM.createRoot(document.getElementById('root')).render(<Homepage />);
