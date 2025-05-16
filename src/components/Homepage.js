dd3f991fcf3dbcd74d6223fb53b30e0a8c74abad
Secursa-Admin <support@secursa.co.za>
2025-05-16 17:22:11 +0200

    Delete src directory

--- src/components/Homepage.js
+++
@@ -1,1 +0,0 @@
-// Final homepage layout goes here
--- src/index.js
+++
@@ -1,1 +0,0 @@
-import React from 'react'; import ReactDOM from 'react-dom/client'; import './index.css'; import Homepage from './components/Homepage'; ReactDOM.createRoot(document.getElementById('root')).render(<Homepage />);
